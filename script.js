c2f1642cabbdec11567c21f264b6689df00975da
Moonesh <shaycser@gmail.com>
2024-03-30 21:50:55 +0400

    Update script.js with new changes

--- script.js
+++ script.js
@@ -9,3 +9,10 @@ console.log (x);
 console.log (y);
 
 console.log (z);
+
+
+if (newDog === "Chihuahua") { 
+    console.log("I love Chihuahuas, it\’s my favorite dog breed");
+} else { 
+    console.log("I dont care, I prefer cats");
+}
